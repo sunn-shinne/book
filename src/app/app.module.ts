2ef37f825ca1ba347b29bbd0e3916cdd6b04c0dc
Ekaterina <brekhova.katya@gmail.com>
2022-07-05 13:02:59 +0300

    setup base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginationComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
