62cba447902e6f6085742244c90aff8144e79970
Ekaterina <brekhova.katya@gmail.com>
2022-07-06 07:45:36 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FifthPageComponent } from './pages/fifth-page/fifth-page.component';
+import { FirstPageComponent } from './pages/first-page/first-page.component';
+import { FourthPageComponent } from './pages/fourth-page/fourth-page.component';
+import { SecondPageComponent } from './pages/second-page/second-page.component';
+import { ThirdPageComponent } from './pages/third-page/third-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/page/1', pathMatch: 'full' },
+  { path: 'page/1', component: FirstPageComponent },
+  { path: 'page/2', component: SecondPageComponent },
+  { path: 'page/3', component: ThirdPageComponent },
+  { path: 'page/4', component: FourthPageComponent },
+  { path: 'page/5', component: FifthPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
+  @Input() pagesCount?: number;
+  selectedPage: number = 1;
+  pagesList: number[] = []
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-
+    this.pagesList = Array(this.pagesCount).fill(0).map((e, i) => i + 1);
   }
 
+  ngDoCheck() {
+    this.setSelectedPage();
+  }
+
+  setSelectedPage() {
+    const splitedUrl = this.router.url.split('/');
+    const currentPage = Number(splitedUrl[splitedUrl.length - 1])
+    this.selectedPage = currentPage;
+  }
 }
